c8a6ecd51244fe4f9141c139f9f0fa0bbd8abb3f
Wenfei Cao <cwfroom123@gmail.com>
2019-09-27 18:59:55 +0900

    Use router

--- web/src/App.js
+++ web/src/App.js
@@ -1,70 +1,34 @@
 import React, {Component} from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import './App.css'
-import PropTypes from 'prop-types'
 import AppBar from '@material-ui/core/AppBar'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
-import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
 import Diary from './containers/Diary'
 import Logbook from './containers/Logbook'
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`tabpanel-${index}`}
-      {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  )
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-}
 
 const apiURL = 'http://127.0.0.1:2638'
 
 class App extends Component {
   constructor (props) {
     super(props)
-    this.state = {
-      currentTab : 0
-    }
   }
-  
-  handleChange = (event, newValue) => {
-    this.setState({
-      currentTab: newValue
-    })
-}
-
 
   render () {
-    
     return (
-      <div className="App">
-      <AppBar position="static">
-        <Tabs value={this.state.currentTab} onChange={this.handleChange}>
-          <Tab label="Diary" value={0}/>
-          <Tab label="Logbook" value={1}/>
-        </Tabs>
-        </AppBar>
-        <TabPanel value={this.state.currentTab} index={0}>
-          <Diary apiURL={apiURL}/>
-        </TabPanel>
-        <TabPanel value={this.state.currentTab} index={1}>
-          <Logbook apiURL={apiURL}/>
-        </TabPanel>
-      </div>
+      <Router>
+        <div className="App">
+        <AppBar position="static">
+          <Tabs value={window.location.pathname}>
+            <Tab href= '/' label="Diary" value='/'/>
+            <Tab href='logbook' label="Logbook" value='/logbook'/>
+          </Tabs>
+          </AppBar>
+          <br />
+          <Route exact path="/" render={()=>(<Diary apiURL={apiURL}/>)}/>
+          <Route exact path="/logbook" render={()=>(<Logbook apiURL={apiURL}/>)}/>
+        </div>
+      </Router>
     )
   }
 }
