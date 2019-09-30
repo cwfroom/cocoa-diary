b3915ec8d2e08492040d8a1255386844d5b87acd
Wenfei Cao <cwfroom123@gmail.com>
2019-09-30 12:05:36 +0900

    Remove unused import

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Route} from "react-router-dom"
 import './App.css'
 import AppBar from '@material-ui/core/AppBar'
 import Tabs from '@material-ui/core/Tabs'
