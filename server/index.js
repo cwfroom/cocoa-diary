db7d7095c8291cb46ab3bbe8b9ce74ec20f6e500
Wenfei Cao <cwfroom123@gmail.com>
2020-04-25 18:13:59 +0900

    Create sample data folder for dev mode

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 app.listen(port, () => {
+    console.log(`Data path: ${config['DataPath']}`)
     console.log(`Listening on port ${port}`)
 })
 
