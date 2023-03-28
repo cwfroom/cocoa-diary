206f43b4902b66d892e718f1234627d249ece0c9
Wenfei Cao <cwfroom123@gmail.com>
2023-03-28 16:58:21 +0900

    Dropdown menu style

--- web/src/components/DropdownMenu.js
+++ web/src/components/DropdownMenu.js
@@ -1,28 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Menu, MenuItem } from '@mui/material';
 
-/*
-const StyledMenu = withStyles({
-  paper: {
-    border: '1px solid #d3d4d5',
-  },
-})(props => (
-  <Menu
-    elevation={0}
-    getContentAnchorEl={null}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'center',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'center',
-    }}
-    {...props}
-  />
-));
-*/
-
 class DropdownMenu extends Component {
 
     constructor (props) {
@@ -64,21 +42,33 @@ class DropdownMenu extends Component {
         return (
             <div>
             <Button
-              variant="contained"
-              color="primary"
-              onClick={this.handleMenuClick}
+                variant="contained"
+                color="primary"
+                onClick={this.handleMenuClick}
             >
               {this.props.selectedValue}
             </Button>
             <Menu
-              id="dropdown-menu"
-              anchorEl={this.state.anchorEl}
-              keepMounted
-              open={Boolean(this.state.anchorEl)}
-              onClose={this.handleMenuClose}
+                elevation={0}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                id="dropdown-menu"
+                anchorEl={this.state.anchorEl}
+                keepMounted
+                open={Boolean(this.state.anchorEl)}
+                onClose={this.handleMenuClose}
             >
                 {items.map( (item, i) => 
-                  <MenuItem key={i} onClick={this.handleItemClick.bind(this, {item})}>{item}</MenuItem>
+                    <MenuItem
+                        key={i}
+                        onClick={this.handleItemClick.bind(this, {item})}>{item}
+                    </MenuItem>
                 )}
             </Menu>
           </div>
