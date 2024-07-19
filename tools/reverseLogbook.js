d58130db64401b6e99913307f7e1491dd92e6e2d
Wenfei Cao <cwfroom123@gmail.com>
2024-07-20 01:08:18 +0900

    Tools: ReverseLogbook

---
+++ tools/reverseLogbook.js
@@ -0,0 +1,22 @@
+'use strict'
+const path = require('path')
+const fs = require('fs')
+
+const logbookPath = '../server/data/logbook'
+const indexFile = JSON.parse(fs.readFileSync(path.resolve(logbookPath, 'index.json')))
+indexFile.forEach(targetfile => {
+    handleLogbook(targetfile)
+})
+
+function handleLogbook(targetFile) {
+    console.log('Logbook: ' + targetFile)
+    let logbookObj = JSON.parse(fs.readFileSync(path.resolve(logbookPath, targetFile + '.json'), {encoding: 'utf-8'}))
+    const subFolder = path.resolve(logbookPath, targetFile)
+    let list = logbookObj['List']
+
+    logbookObj['List'] = list.reverse()
+    fs.writeFileSync(
+        path.resolve(logbookPath, targetFile+'.json'),
+        JSON.stringify(logbookObj, null, '\t')
+    )
+}
