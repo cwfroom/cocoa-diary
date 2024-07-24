569662de18c414c73b547f5f368e111c6d571fc7
Wenfei Cao <cwfroom123@gmail.com>
2024-07-24 12:50:09 +0900

    Improved registerAnime script

--- tools/registerAnime.js
+++ tools/registerAnime.js
@@ -14,7 +14,9 @@ Output format
     "Title": "AnimeTitle",
     "Release": "2023春",
     "Total": "12"
-    "Watched": 12
+    "Watched": "12",
+    "AirDay": "月",
+    "AirTime": "2200",
     "Date": Date
 }
 */
@@ -24,30 +26,44 @@ const outputFileName = "registerAnimeOutput.json"
 
 let lines = fs.readFileSync(path.resolve(__dirname, inputFileName), {encoding: "utf-8"}).split('\r\n')
 
-const release = lines[0]
+const release = lines[0].replace('アニメ', '')
 let result = []
+let airDay = ''
 for (let i = 1; i < lines.length; i++)
 {
     let line = lines[i]
-    if (line.length > 1){
+    if (lines[i].length === 1) {
+        airDay = lines[i]
+    }else {
         let lineParts = line.split(/\s/)
-        if (lineParts.length === 4)
-        {
-            let episodes = lineParts[2].replace('終', '')
-            result.push({
-                "Title": lineParts[1],
-                "Release": release,
-                "Total": episodes,
-                "Watched": episodes,
-                "Date": lineParts[3]
-            })
-        }else
-        {
-            //Print to handle manually
-            console.log(lineParts)
+        let hasDate = lineParts[lineParts.length-1].length === 8
+        let titleArr = []
+        for (let i = 1; i < lineParts.length - 2; i++) {
+            titleArr.push(lineParts[i])
         }
+        let date = ""
+        let episodes = ""
+        if (!hasDate) {
+            titleArr.push(lineParts[lineParts.length - 2])
+            episodes = lineParts[lineParts.length - 1].replace('終', '')
+        }else {
+            date = lineParts[lineParts.length - 1]
+            episodes = lineParts[lineParts.length - 2].replace('終', '')
+        }
+        const title = titleArr.join(' ')
+
+        result.push({
+                "Title": title,
+                "Release": release,
+                "Total": '12',
+                "Watched": episodes,
+                "AirDay": airDay,
+                "AirTime": lineParts[0],
+                "Date": date
+        })
 
     }
 }
 
-fs.writeFileSync(path.resolve(__dirname, outputFileName), JSON.stringify(result, null, 4))
+const wrapped = {[release] : result}
+fs.writeFileSync(path.resolve(__dirname, outputFileName), JSON.stringify(wrapped, null, 4))
